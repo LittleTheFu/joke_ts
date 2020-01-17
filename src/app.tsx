2845b76d6a209ea93d59a761fd9118ce8a5e7c87
fuyuxiang <lttlstpd@live.com>
2020-01-17 20:57:30 +0800

    intruduce eslint

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 let color: string = "blue";
+let good = 3;
 ReactDOM.render(
     <div>{color}</div>,
     document.getElementById('root')
