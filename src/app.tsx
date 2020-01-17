00dfe1fef9d6a3dfa43b3f084b48541bf3a5c5ce
fuyuxiang <lttlstpd@live.com>
2020-01-17 21:25:35 +0800

    fix

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 const color = "blue";
-const good === 3;
 ReactDOM.render(
     <div>{color}</div>,
     document.getElementById('root')
