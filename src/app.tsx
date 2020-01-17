efb8c63efad6fad9794822916ba04dd275987b02
fuyuxiang <lttlstpd@live.com>
2020-01-17 21:23:03 +0800

    test hsky

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 const color = "blue";
-// const good = 3;
+const good ==== 3;
 ReactDOM.render(
     <div>{color}</div>,
     document.getElementById('root')
