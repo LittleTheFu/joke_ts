d11707f91e5db996512b67fbedd6f1fd28daae0e
fuyuxiang <lttlstpd@live.com>
2020-01-18 13:39:54 +0800

    test

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-var color = "blue";
+const color = "blue";
 ReactDOM.render(
     <div>{color}</div>,
     document.getElementById('root')
