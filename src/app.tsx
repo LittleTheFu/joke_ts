e30466148fb27c664a868e1e5da95550932b3d00
fuyuxiang <lttlstpd@live.com>
2020-01-18 13:35:40 +0800

    test pre commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const color = "blue";
+let color = "blue";
 ReactDOM.render(
     <div>{color}</div>,
     document.getElementById('root')
