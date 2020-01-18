6426cede664b86afcef32d0d0ed26d01dbea16ab
fuyuxiang <lttlstpd@live.com>
2020-01-18 14:34:39 +0800

    introduce material-ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 
 const color = 'blue';
-ReactDOM.render(<div>{color}</div>, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        {color}
+        <Button variant="contained" color="primary">
+            你好，世界
+        </Button>
+    </div>,
+    document.getElementById('root'),
+);
