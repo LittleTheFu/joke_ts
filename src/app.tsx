00788bd35d57de674ec11cea21f2ca73cc8f6396
fuyuxiang <lttlstpd@live.com>
2020-01-18 15:20:32 +0800

    try material-ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const color = 'blue';
 ReactDOM.render(
@@ -9,6 +13,12 @@ ReactDOM.render(
         <Button variant="contained" color="primary">
             你好，世界
         </Button>
+        <FormGroup>
+            <FormControlLabel control={<Switch size="small" />} label="Small" />
+            <FormControlLabel control={<Switch />} label="Normal" />
+        </FormGroup>
+        <CircularProgress />
+        <CircularProgress color="secondary" />
     </div>,
     document.getElementById('root'),
 );
