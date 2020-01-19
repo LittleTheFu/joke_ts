91d212b922cb83b3bc9c31de2dec3da115919d05
fuyuxiang <lttlstpd@live.com>
2020-01-19 21:11:50 +0800

    test double parmas

--- src/app.tsx
+++ src/app.tsx
@@ -10,7 +10,7 @@ import JokeApp from './jokeApp';
 const color = 'blue';
 ReactDOM.render(
     <div>
-        <JokeApp name="Jake" />
+        <JokeApp name="Jake" color="yellow" />
         {color}
         <Button variant="contained" color="primary">
             你好，世界
--- src/jokeApp.tsx
+++ src/jokeApp.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
-// import * as PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-// export interface Props {
-//     name: string;
-// }
-
-// type Props = { name: string };
-
-const JokeApp: React.FC<{ name: string }> = ({ name }) => {
-    return <div>{name}</div>;
+const JokeApp: React.FC<{ name: string; color: string }> = ({ name, color }) => {
+    return (
+        <div>
+            {name} with {color}
+            <Button variant="contained" color="primary">
+                你好，世界
+            </Button>
+            <FormGroup>
+                <FormControlLabel control={<Switch size="small" />} label="Small" />
+                <FormControlLabel control={<Switch />} label="Normal" />
+            </FormGroup>
+            <CircularProgress />
+            <CircularProgress color="secondary" />
+        </div>
+    );
 };
-
-// JokeApp.propTypes = {
-//     name: PropTypes.string,
-// };
 
 export default JokeApp;
