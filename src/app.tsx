1e1dbefb17ae76cc0f9827490072ca80999d5a12
fuyuxiang <lttlstpd@live.com>
2020-01-20 14:39:45 +0800

    clear code

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import JokeApp from './jokeApp';
 
-const color = 'blue';
 ReactDOM.render(
     <div>
-        <JokeApp name="Jake" />
-        {color}
-        <Button variant="contained" color="primary">
-            你好，世界
-        </Button>
-        <FormGroup>
-            <FormControlLabel control={<Switch size="small" />} label="Small" />
-            <FormControlLabel control={<Switch />} label="Normal" />
-        </FormGroup>
-        <CircularProgress />
-        <CircularProgress color="secondary" />
+        <JokeApp />
     </div>,
     document.getElementById('root'),
 );
--- src/jokeApp.tsx
+++ src/jokeApp.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import Button from '@material-ui/core/Button';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-import CircularProgress from '@material-ui/core/CircularProgress';
+// import FormGroup from '@material-ui/core/FormGroup';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Switch from '@material-ui/core/Switch';
+// import CircularProgress from '@material-ui/core/CircularProgress';
 
 // interface Props {
 //     name: string;
@@ -28,19 +28,29 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 // };
 
 //####################################################################################################
-const JokeApp: React.FC<{ name: string; word?: string }> = ({ name, word = 'pol' }) => {
+// const JokeApp: React.FC<{ name: string; word?: string }> = ({ name, word = 'pol' }) => {
+//     return (
+//         <div>
+//             {name} with {word}
+//             <Button variant="contained" color="primary">
+//                 你好，世界
+//             </Button>
+//             <FormGroup>
+//                 <FormControlLabel control={<Switch size="small" />} label="Small" />
+//                 <FormControlLabel control={<Switch />} label="Normal" />
+//             </FormGroup>
+//             <CircularProgress />
+//             <CircularProgress color="secondary" />
+//         </div>
+//     );
+// };
+
+const JokeApp: React.FC = () => {
     return (
         <div>
-            {name} with {word}
             <Button variant="contained" color="primary">
-                你好，世界
+                joke
             </Button>
-            <FormGroup>
-                <FormControlLabel control={<Switch size="small" />} label="Small" />
-                <FormControlLabel control={<Switch />} label="Normal" />
-            </FormGroup>
-            <CircularProgress />
-            <CircularProgress color="secondary" />
         </div>
     );
 };
