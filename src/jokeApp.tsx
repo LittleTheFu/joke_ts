cf11e10f28be48570afc7bf491dacdfbc34d6aaf
fuyuxiang <lttlstpd@live.com>
2020-01-21 17:05:20 +0800

    try tooltip

--- src/jokeApp.tsx
+++ src/jokeApp.tsx
@@ -4,6 +4,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import Tooltip from '@material-ui/core/Tooltip';
 import { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -114,6 +115,10 @@ const JokeApp: React.FC = () => {
             });
     };
 
+    const hoverThumbers = (): void => {
+        console.log('hover thubmers');
+    };
+
     return (
         <div>
             <Grid container justify="center" alignItems="center" className={classes.root}>
@@ -125,10 +130,16 @@ const JokeApp: React.FC = () => {
                     )}
                 </Grid>
                 <Grid item xs={6} sm={3}>
-                    <ThumbUpIcon className={classes.thumbers} />:{upvotes}
+                    <Tooltip title="Voteup">
+                        <ThumbUpIcon onMouseEnter={hoverThumbers} className={classes.thumbers} />
+                    </Tooltip>
+                    :{upvotes}
                 </Grid>
                 <Grid item xs={6} sm={3}>
-                    <ThumbDownIcon className={classes.thumbers} />:{downvotes}
+                    <Tooltip title="Votedown">
+                        <ThumbDownIcon onMouseEnter={hoverThumbers} className={classes.thumbers} />
+                    </Tooltip>
+                    :{downvotes}
                 </Grid>
                 <Grid item xs={12} sm={6}>
                     <Button variant="contained" color="primary" onClick={fetchData}>
